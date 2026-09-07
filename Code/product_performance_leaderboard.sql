e97d3bdc004f0fc15010b13c476ef5f1e43f4144
sushma <sushma.tacholi@gmail.com>
2026-09-07 15:46:36 -0700

    updates

---
+++ Code/product_performance_leaderboard.sql
@@ -0,0 +1,11 @@
+-- product performnace leaderboard
+SELECT 
+	product_title, 
+	total_sales, 
+	net_items_sold, 
+	ROUND(total_sales/net_items_sold,2) AS AOV,
+	ROW_NUMBER() OVER (ORDER BY total_sales DESC) AS revenue_rank
+FROM shopify_sales
+WHERE net_items_sold > 0
+ORDER BY total_sales DESC
+LIMIT 25;
